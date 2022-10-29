e9bb9f739e69983a8075f07bc3436d64c4d5d0c3
bmokr <56951744+bmokr@users.noreply.github.com>
2022-10-29 16:00:29 +0200

    0.1.3
    -base build
    -timeout service win/unix (unix not manged yet)
    -service can edit deposit
    -timeout BUGGED 2ND ACCESS
    ###
    -change deposit to obj
    -manage product obj (prod doubled)

--- header.py
+++ header.py
@@ -1,13 +1,12 @@
 import traceback
 import machine as m
+
+
 def run():
     start = True # kondycja głównej pętli
 
     try:    # dodanie maszyny
         coffeMachine = m.Machine()
-        # coffeMachine.set_products()
-        # zamienić na wczytanie pliku print(str(coffeMachine.get_products(input("Input name of product: "))))
-
     except:
         print(traceback.format_exc())
 
--- machine.py
+++ machine.py
@@ -1,6 +1,8 @@
 import product as p
 import random
-from interruptingcow import timeout  # https://pypi.org/project/interruptingcow/
+# from interruptingcow import timeout  # https://pypi.org/project/interruptingcow/
+# no signal on win os module 'signal' has no attribute 'SIGALRM'
+import timeout
 
 
 class Machine:
@@ -9,7 +11,7 @@ class Machine:
         self.products = {}
         self.money = {"pln": 0, "usd": 0}
 
-    def money(self, password):
+    def moneyGet(self, password) -> object:
         if password & self.money["pln"] != 0 | self.money["usd"] != 0:
             print("Withdraw: \nPLN: {}.\nUSD: {}.\n".format(self.money["pln"], self.money["usd"]))
             self.money.update({"pln": 0, "usd": 0})
@@ -19,45 +21,77 @@ class Machine:
             else:
                 print("No money.")
 
-    def money(self, password, pln, usd):
+    def moneySet(self, password, pln, usd):
         if password:
             self.money.update({"pln": pln, "usd": usd})
             print("Machine deposit is now updated.")
         else:
             print("Access denied.")
 
-    def get_products(self, name):
+    def getProducts(self, name): # get product price
         return self.products.get(name)
 
-    def set_products(self):
-        name = input("Input product name: ")
-        ingredients = input("Input product ingredients (like *, *): ")
-        cost = input("Input product cost: ")
-        self.products.update({name: p.Product(ingredients, cost)})
+    def setProducts(self, password): # update or set product
+        if password:
+            name = input("Input product name: ")
+            ingredients = input("Input product ingredients (like *, *): ") # ???
+            cost = input("Input product cost: ")
+            self.products.update({name: p.Product(ingredients, cost)})
+        else:
+            print("Access denied.")
 
     ##############################################################################################
+    # @ co to, poczytac
+    @timeout.timeout(60)
+    def serviceTask(cls):
+        serwisJob = input("1 - take out money\n2 - set money\n3 - update product\n")
+        if serwisJob == "1":
+            cls.moneyGet(True)
+        elif serwisJob == "2":
+            plnIn = input("Input pln: ")
+            usdIn = input("Input usd: ")
+            cls.moneySet(True, plnIn, usdIn)
+        elif serwisJob == "3":
+            cls.setProducts(1)
+        else:
+            pass
 
-    @staticmethod  # co to poczytac
-    def switch(case):
+    def switch(cls, case):
         if case == "serwis":
             x = random.randint(0, 9)
-            print()
             if input(str(x)) == str(x + 1):
-                print("You have 60 sek.\n")
+                print("You have 10 sek.\n")
                 try:
-                    with timeout(60, exception=RuntimeError):  # stops after 60 sek
-                        serwisJob = input("1 - take out money")
-                except RuntimeError:
-                    print
-                    "didn't finish within 60 seconds"
+                    cls.serviceTask()
+                except:
+                    print("didn't finish within 10 seconds")
+                # try:
+                #     with timeout(10):  # stops after 10 sek
+                #         serwisJob = input("1 - take out money\n2- set money\n3-update product\n")
+                #         if serwisJob == "1":
+                #             cls.moneyGet(1)
+                #         elif serwisJob == "2":
+                #             plnIn = input("Input pln: ")
+                #             usdIn = input("Input usd: ")
+                #             cls.moneySet(1, plnIn, usdIn)
+                #         elif serwisJob == "3":
+                #             cls.setProducts(1)
+                #         else:
+                #             pass
+                # except RuntimeError:
+                #     print("didn't finish within 10 seconds")  # działa tylko w unix interruptingcow
             else:
-                print("serwis nie zostal przyznany")
+                print("Service not granted.\n")
             return 1
         elif case == "stop":
             return "exit"
-        elif case == "3":
-            return "You can become a Data Scientist"
+        elif case == "1":
+            for k, v in cls.products
+            coffeeCase = input("Pic up a coffe: ")
         elif case == "4":
             return "You can become a Blockchain developer."
         elif case == "5":
             return "You can become a mobile app developer"
+        else:
+            print("There is no such number.")
+            return 1
